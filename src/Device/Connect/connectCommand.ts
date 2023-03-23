8d0dc17f1a6516a662b27fc958c2d6c67bb1360a
Michael Zabka <zabka.michael@gmail.com>
2023-03-23 09:14:28 +0000

    The sos device connect command will start applet server is it did earlier
    and it uses the idempotent API of SDK so it can cooperate with all other tools using the same api
    it will run the applet serve if not started yet
    it will reuse existing running server if other sdk-based tool already started the applet server
    so there is no requirement on order of starting commands

--- src/Device/Connect/connectCommand.ts
+++ src/Device/Connect/connectCommand.ts
@@ -4,7 +4,6 @@ import { APPLET_UID_OPTION, getAppletUid, getAppletVersion } from "../../Applet/
 import { createOrganizationRestApi } from "../../helper";
 import { CommandLineOptions, createCommandDefinition } from "../../Command/commandDefinition";
 import { createDevelopment } from "@signageos/sdk";
-import { getMachineRemoteAddr } from '@signageos/sdk/dist/Utils/network';
 import wait from "../../Timer/wait";
 
 const SERVER_PUBLIC_URL_OPTION = {
@@ -46,13 +45,19 @@ export const connect = createCommandDefinition({
 		const appletVersion = await getAppletVersion(currentDirectory);
 		const deviceUid = await getDeviceUid(restApi, options);
 
-		const appletPort = options[SERVER_PORT_OPTION.name] ?? await dev.applet.serve.getRunningPort(appletUid, appletVersion);
-		const appletPublicUrl = options[SERVER_PUBLIC_URL_OPTION.name] ?? `http://${getMachineRemoteAddr()}:${appletPort}`;
+		const appletPort = options[SERVER_PORT_OPTION.name];
+		const appletPublicUrl = options[SERVER_PUBLIC_URL_OPTION.name];
 
+		const server = await dev.applet.serve.serve({
+			appletUid,
+			appletVersion,
+			port: appletPort,
+			publicUrl: appletPublicUrl,
+		});
 		const connection = await dev.deviceConnect.connect(deviceUid, {
 			appletUid,
 			appletVersion,
-			appletPublicUrl,
+			appletPublicUrl: server.publicUrl,
 		});
 
 		const stopServer = async () => {
