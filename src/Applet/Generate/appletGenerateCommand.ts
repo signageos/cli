7ee97b312fa79c194208342e195046b0008de24c
Mikuláš Sychra <mikulas.sychra@gmail.com>
2025-03-25 14:18:44 +0100

    Fixed example to pass strict checking

--- src/Applet/Generate/appletGenerateCommand.ts
+++ src/Applet/Generate/appletGenerateCommand.ts
@@ -378,8 +378,10 @@ import sos from '@signageos/front-applet';
 // Wait on sos data are ready (https://docs.signageos.io/api/js/content/latest/js-applet-basics#onready)
 sos.onReady().then(async function () {
 	const contentElement = document.getElementById('root');
-	console.log('sOS is ready');
-	contentElement.innerHTML = 'sOS is ready';
+	if (contentElement) {
+		console.log('sOS is ready');
+		contentElement.innerHTML = 'sOS is ready';
+	}
 });
 `;
 
