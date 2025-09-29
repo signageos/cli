550029e1cbaff3b383f1962c09c0ae779ef3835d
Mikuláš Sychra <mikulas.sychra@gmail.com>
2025-09-29 14:00:17 +0200

    Removed --legacy-enabled login

--- tests/unit/Auth/loginCommand.spec.ts
+++ tests/unit/Auth/loginCommand.spec.ts
@@ -1,39 +1,18 @@
 import should from 'should';
-import { getIsAuth0OrLegacyEnabled } from '../../../src/Auth/loginCommand';
+import { getIsAuth0Enabled } from '../../../src/Auth/loginCommand';
 
 describe('Auth.loginCommand', () => {
-	describe('getIsAuth0OrLegacyEnabled', () => {
+	describe('getIsAuth0Enabled', () => {
 		it('should return true if --auth0-enabled is present', () => {
-			const { isAuth0Enabled } = getIsAuth0OrLegacyEnabled({ _unknown: ['--auth0-enabled'], command: ['login'] });
+			const { isAuth0Enabled } = getIsAuth0Enabled({ _unknown: ['--auth0-enabled'], command: ['login'] });
 
 			should(isAuth0Enabled).be.true();
 		});
 
-		it('should return true if --legacy-enabled is present', () => {
-			const { isLegacyEnabled } = getIsAuth0OrLegacyEnabled({ _unknown: ['--legacy-enabled'], command: ['login'] });
-
-			should(isLegacyEnabled).be.true();
-		});
-
 		it('should return undefined (server default) if --auth0-enabled is NOT present', () => {
-			const { isAuth0Enabled } = getIsAuth0OrLegacyEnabled({});
+			const { isAuth0Enabled } = getIsAuth0Enabled({});
 
 			should(isAuth0Enabled).be.undefined();
-		});
-
-		it('should return undefined (server default) if --legacy-enabled is NOT present', () => {
-			const { isLegacyEnabled } = getIsAuth0OrLegacyEnabled({});
-
-			should(isLegacyEnabled).be.undefined();
-		});
-
-		it('should fail if both --auth0-enabled and --legacy-enabled are present', () => {
-			should(() =>
-				getIsAuth0OrLegacyEnabled({
-					_unknown: ['--auth0-enabled', '--legacy-enabled'],
-					command: ['login'],
-				}),
-			).throwError('Only one override from auth0 and legacy authentication options can be active at the moment.');
 		});
 	});
 });
